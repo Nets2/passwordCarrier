d7ff4db0835cb8d46763bceb5421d67af1058286
Geoffrey <geo.bisch@gmail.com>
2020-08-21 17:28:55 +0200

    adding subscribe method

--- passwordCarrier/passwordCarrier/Form3.cs
+++ passwordCarrier/passwordCarrier/Form3.cs
@@ -1,14 +1,81 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace passwordCarrier
 {
     public partial class Form3 : Form
     {
         private Form1 Parent { get; set; }
+        private bool FileExistFlag { get; set; }
         public Form3( Form1 parent)
         {
             this.Parent = parent;
             InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            Parent.WindowState = FormWindowState.Minimized;
+            this.BringToFront();
+            this.Activate();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Parent.Visible = true;
+            this.Parent.WindowState = FormWindowState.Normal;
+            this.Parent.BringToFront();
+            this.Close();
+        }
+
+        private void validateButton_Click(object sender, EventArgs e)
+        {
+            string login = this.loginTextArea.Text;
+            string password = this.passwordTextArea.Text;
+            string path = @"../../data/";
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                string finalPath = path + Security.GetHash(sha256Hash, login);
+                try
+                {
+                    //on vérifie que le fichier existe
+                    //s'il existe on passe le flag a true;
+                    StreamReader sr = File.OpenText(finalPath);
+                    FileExistFlag = true;
+                }
+                catch
+                {
+                    //si ce n'est pas le cas on le créer et on passe le flag a false
+                    using (FileStream fs = File.Create(finalPath))
+                    {
+                        byte[] info = new UTF8Encoding(true).GetBytes(password);
+                        // Add some information to the file.
+                        fs.Write(info, 0, info.Length);
+                    }
+                    FileExistFlag = false;
+                }
+                finally
+                {                    
+                    //dans tout les cas
+                    if (FileExistFlag)
+                    {
+                        string message = "Error: username already in use";
+                        string caption = "Error";
+                        MessageBoxButtons buttons = MessageBoxButtons.OK;
+                        DialogResult result;
+
+                        // Displays the MessageBox.
+                        result = MessageBox.Show(message, caption, buttons);
+                    }
+                    else
+                    {
+                        cancelButton_Click(this, EventArgs.Empty);
+                    }
+                }
+            }
         }
     }
 }
