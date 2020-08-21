a3cf94bc689896cea51308d90b35f5512572bb09
Geoffrey <geo.bisch@gmail.com>
2020-08-21 11:56:22 +0200

    adding form3
    form3 will be used to create new account on password manager

---
+++ passwordCarrier/passwordCarrier/Form3.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace passwordCarrier
+{
+    public partial class Form3 : Form
+    {
+        private Form1 Parent { get; set; }
+        public Form3( Form1 parent)
+        {
+            this.Parent = parent;
+            InitializeComponent();
+        }
+    }
+}
