39ec374406c2a1317c0949b8112859bf4115766c
Geoffrey <geo.bisch@gmail.com>
2020-08-27 23:16:13 +0200

    First version of data structure

--- passwordCarrier/passwordCarrier/Account.cs
+++ passwordCarrier/passwordCarrier/Account.cs
@@ -1,7 +1,29 @@
-﻿namespace passwordCarrier
+﻿using System.ComponentModel;
+using System.Runtime.Remoting.Activation;
+
+namespace passwordCarrier
 {
     public class Account
     {
+        /* ItemName : url : Login : password : e-mail : question secrete : réponse */
+        public Account(string itemName,string email,  PasswordPropertyTextAttribute password, UrlAttribute url=null, string question="", string answer="")
+        {
+            ItemName = itemName;
+            Url = url;
+            Password = password;
+            Email = email;
+            Question = question;
+            Answer = answer;
+        }
+
+        public string ItemName { get; private set; }
+        public UrlAttribute Url { get; private set; }
+        public PasswordPropertyTextAttribute Password { get; private set; }
+        public string Email { get; private set; }
+        public string Question { get; private set; }
+        public string Answer { get; private set; }
+        
+        
         
     }
 }
